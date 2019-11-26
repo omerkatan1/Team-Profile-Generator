00f177c8348b9c13f96cb54720aaceb32aa4288d
Omer Katan <omerkatan@live.com>
2019-11-26 03:33:31 -0800

    classes files added

---
+++ Classes/employee-class.js
@@ -0,0 +1,9 @@
+class Employee {
+
+    constructor(name, id, title) {
+        this.name = name;
+        this.id = id;
+        this.title = title;
+    }
+
+}
--- app.js
+++ app.js
@@ -4,8 +4,26 @@ const inquirer = require('inquirer');
 const questions = [
     {
         type: 'list',
-        name: 'typeList',
+        name: 'employeeType',
         message: 'What is your employee type?',
         choices: ['Intern', 'Engineer', 'Manager']
+    },
+    {
+        type: 'message',
+        message: 'What is your employees email?',
+        name: 'email'
+    },
+    {
+        type: 'message',
+        message: 'Input your employees ID:',
+        name: 'id'
     }
 ]
+
+
+
+inquirer.prompt(questions).then(answers => {
+    console.log(answers.employeeType);
+    console.log(answers.email);
+    console.log(answers.id);
+})
