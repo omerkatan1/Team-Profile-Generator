a8533caa18ee7c4b47af7c523365291c674bcddd
Omer Katan <omerkatan@live.com>
2019-11-26 03:40:57 -0800

    employee class

--- Classes/employee-class.js
+++ Classes/employee-class.js
@@ -1,9 +1,22 @@
 class Employee {
 
-    constructor(name, id, title) {
+    constructor(name, id, role, email) {
         this.name = name;
         this.id = id;
-        this.title = title;
+        this.email = email;
+
+        function getName(){
+            console.log(`Employee name: ${this.name}`);
+        }
+        function getID() {
+            console.log(`Employee ID: ${this.id}`);
+        }
+        function getRole(){
+            console.log(`Employee role: Employee`);
+        }
+        function getEmail(){
+            console.log(`Employee email: ${this.email}`);
+        }
     }
 
 }
