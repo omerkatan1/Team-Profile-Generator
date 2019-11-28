0bc5ae96410b74c9653139d763277e1cf642b06f
Omer Katan <omerkatan@live.com>
2019-11-28 03:50:30 -0800

    js

---
+++ output/assets/build.js
@@ -0,0 +1,16 @@
+
+
+
+const employeeArray = [];
+
+function createEmployeeBox() {
+
+    const column = $('<div>');
+    column.addClass('col-6');
+
+    const container = $('<div>');
+    container.addClass('employee-container');
+
+}
+
+
