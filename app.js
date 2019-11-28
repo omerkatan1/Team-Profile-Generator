be6918dfb76a144ae760ec44a1d95735ab31b11e
Omer Katan <omerkatan@live.com>
2019-11-27 16:55:55 -0800

    engineer class

--- app.js
+++ app.js
@@ -73,7 +73,13 @@ inquirer.prompt(questions).then(answers => {
         });
     } else if (type === 'Engineer') {
         inquirer.prompt(engineerQuestions).then(engineerAnswers => {
+            const github = engineerAnswers.employeeGitHub;
+            const test2 = new Engineer(name, id, email, github);
 
+            console.log(test2.getGithub());
+            test.getName();
+            test.getId();
+            test.getRole();
         });
     } else if (type === 'Mangager') {
         inquirer.prompt(managerQuestions).then(managerAnswers => {
