f084e7db142e928103aa162db88b0d88c359323c
Omer Katan <omerkatan@Omers-MacBook-Pro.local>
2020-03-05 22:54:15 -0800

    push

--- app.js
+++ app.js
@@ -1,7 +1,126 @@
 const inquirer = require('inquirer');
 const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const Manager = require('./lib/manager');
 
-let test = "HEllo";
-console.log(test);
-console.log(test.toLowerCase());
+employeeHolder = [];
 
+function runApp() {
+
+    inquirer.prompt([
+        {
+            name: 'employeeType',
+            type: 'list',
+            message: 'Choose a type of employee you would like to add:',
+            choices: ['Intern', 'Engineer', 'Manager']
+        }
+    ]).then(function (answer) {
+        switch (answer.employeeType) {
+            case 'Intern':
+                promptIntern();
+                break;
+
+            case 'Engineer':
+                promptEngineer();
+                break;
+
+            case 'Manager':
+                promptManager();
+                break;
+        }
+    })
+
+
+    function promptIntern() {
+        inquirer.prompt([
+            {
+                name: 'Name',
+                type: 'input',
+                message: 'What is your interns name?'
+            },
+            {
+                name: 'id',
+                type: 'input',
+                message: 'What is your interns ID number?'
+            },
+            {
+                name: 'School',
+                type: 'input',
+                message: 'what school did your intern go to?'
+            }
+        ]).then(function (answer) {
+            const newIntern = new Intern(answer.Name.toLowerCase(), answer.id, answer.School);
+            employeeHolder.push(newIntern);
+            addOrQuit();
+        });
+    }
+
+    function promptEngineer() {
+        inquirer.prompt([
+            {
+                name: 'Name',
+                type: 'input',
+                message: 'What is your engineers name?'
+            },
+            {
+                name: 'id',
+                type: 'input',
+                message: 'What is your engineers ID number?'
+            },
+            {
+                name: 'github',
+                type: 'input',
+                message: 'What is your engineers github?'
+            }
+        ]).then(function (answer) {
+            const newEngineer = new Engineer(answer.Name.toLowerCase(), answer.id, answer.github);
+            employeeHolder.push(newEngineer);
+            addOrQuit();
+        });
+    }
+
+    function promptManager() {
+        inquirer.prompt([
+            {
+                name: 'Name',
+                type: 'input',
+                message: 'What is your managers name?'
+            },
+            {
+                name: 'id',
+                type: 'input',
+                message: 'What is your managers ID number?'
+            },
+            {
+                name: 'officeNumber',
+                type: 'input',
+                message: 'what is your managers office number?'
+            }
+        ]).then(function (answer) {
+            const newManager = new Manager(answer.Name.toLowerCase(), answer.id, answer.officeNumber);
+            employeeHolder.push(newManager);
+            addOrQuit();
+        });
+    }
+
+
+    function addOrQuit() {
+        inquirer.prompt({
+            name: 'addOrQuit',
+            type: 'list',
+            message: 'Would you like to add another employee or start generating the Template?',
+            choices: ['Add', 'Generate']
+        }).then(function (answer) {
+            switch (answer.addOrQuit) {
+                case "Add":
+                    runApp();
+                    break;
+
+                case "Generate":
+                    break;
+            }
+        })
+    }
+}
+
+runApp();
