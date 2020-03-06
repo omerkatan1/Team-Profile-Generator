ea3fadca28486d515b4e23b39e10b1412b98e2ad
Omer Katan <omerkatan@Omers-MacBook-Pro.local>
2020-03-05 22:14:07 -0800

    engineer

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-const Employee = require('./lib/employee');
 const inquirer = require('inquirer');
+const Engineer = require('./lib/engineer');
+
+let test = "HEllo";
+console.log(test);
+console.log(test.toLowerCase());
+
